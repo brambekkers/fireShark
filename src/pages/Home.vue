c6d4fdd12aeebe20f79f70f3e51d7a87b9380944
BramBekkers <bram.bekkers@outlook.com>
2024-02-05 14:41:16 +0100

    small button style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -52,7 +52,7 @@ fetchUsers();
       <div class="flex align-center justify-center mt-12">
         <router-link to="/practice">
           <button
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
             :disable="!selectedTopics.length"
           >
             start
