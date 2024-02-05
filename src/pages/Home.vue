1bab3cf32f1a6520c7e82da0372df725bd8f969b
Kongonon <33282458+kenanamat@users.noreply.github.com>
2024-02-05 16:09:35 +0100

    fix percentage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -51,7 +51,7 @@ watchEffect(() => {
         <div>
           <p class="text-center mt-8">
             Your performance is at
-            <span class="text-2xl font-bold text-blue-700">{{ percentage }}%</span>
+            <span class="text-2xl font-bold text-blue-700">{{ userStore.stats.percentage }}%</span>
           </p>
         </div>
       </section>
