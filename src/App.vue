78acb9c532620c03db661f9cdfc0d4c125cb84a3
Yesim <yesim.oguz@nl.abnamro.com>
2024-02-05 13:05:26 +0100

    removed importing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
   import MainLayout from '@/layouts/MainLayout.vue'
-  import QuestionPage from './pages/QuestionPage.vue';
 </script>
 
 <template>
   <MainLayout class="bg-sharp-body-bg">
     <router-view />
-    <QuestionPage/>
   </MainLayout>
 </template>
