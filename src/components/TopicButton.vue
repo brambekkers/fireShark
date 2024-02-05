f7bc10e66267c2f941e0dae9dea4ee1ff5f03444
Ali Gok <ali.goek@nl.abnamro.com>
2024-02-05 15:30:22 +0100

    fixed the icon size

--- src/components/TopicButton.vue
+++ src/components/TopicButton.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { storeToRefs } from 'pinia';
 import { ref, watch } from 'vue';
-import IconCheck from '~icons/material-symbols/trophy-outline';
-
 import { useQuestionStore } from '@stores/question';
+import IconCheck from '~icons/lucide/check';
 
 const props = defineProps({
   title: { type: String, required: true },
@@ -31,17 +30,12 @@ watch(isChecked, (check) => {
   >
     <!-- header -->
     <div class="flex relative">
-      <!-- background -->
-      <div class="bg-white rounded-tl-2xl min-h-6 flex-1"></div>
-      <div class="min-h-6 min-w-6"></div>
-      <div class="bg-white rounded-tr-2xl min-h-6 flex-1"></div>
-
       <!-- check circle -->
       <div
-        class="bg-slate-200 min-h-6 min-w-6 absolute z-10 rounded-full p-4"
+        class="bg-slate-200 absolute z-10 rounded-full p-4 max-h-1 max-w-1 flex justify-center"
         :class="isChecked ? 'checked' : 'circle'"
       >
-        <span v-if="isChecked"><IconCheck /></span>
+        <span v-if="isChecked" class="icon"><IconCheck /></span>
       </div>
     </div>
     <!-- Title -->
@@ -54,16 +48,21 @@ watch(isChecked, (check) => {
         class="hidden"
         type="checkbox"
         aria-labelledby="selected"
-      />
+      >
       <h3 class="text-xl text-center font-bold text-primary">
-        {{ title }} {{ isChecked ? 'on' : 'off' }}
+        {{ title }}
       </h3>
-      <div class="mt-6 h-2 rounded bg-slate-200 relative overflow-hidden">
-        <div
-          :style="{ width: `${progress}%` }"
-          class="from-primary to-secondary bg-gradient-to-r h-full"
-        ></div>
-      </div>
+      <section class="percentage-section">
+        <div class="mt-6 h-2 rounded bg-slate-200 relative overflow-hidden">
+          <div
+            :style="{ width: `${progress}%` }"
+            class="from-primary to-secondary bg-gradient-to-r h-full"
+          ></div>
+        </div>
+        <p class="percentage">
+          {{ progress }}%
+        </p>
+      </section>
     </div>
   </div>
 </template>
@@ -80,4 +79,15 @@ watch(isChecked, (check) => {
   background-color: #1fc364;
   border: 0.4rem solid #f2f9f9;
 }
+.icon {
+  color: #ffffff;
+  font-size: 1rem;
+  align-self: center;
+}
+.percentage {
+  color: #009286;
+  font-weight: 700;
+  line-height: 1.2;
+}
+
 </style>
