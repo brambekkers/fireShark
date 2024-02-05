5d0677f2dcd735c50c53d20713da1c9c723326f7
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-02-05 13:10:43 +0100

    Added all html - logic is failing atm

---
+++ src/components/questions/Answer.vue
@@ -0,0 +1,15 @@
+<template>
+  <input :type="inputType" :name="questionId" :id="questionId" :value="value">
+  <label :for="questionId">{{ text }}</label>
+</template>
+
+<script setup>
+  defineProps({
+    answer: Object,
+    type: String,
+  })
+
+  const inputType = this?.type === 'singleChoice' ? 'radio' : 'checkbox';
+  const {text, value} = this.answer;
+  const questionId = `q-${this?.text}`;
+</script>
--- src/pages/QuestionPage.vue
+++ src/pages/QuestionPage.vue
@@ -1,8 +1,11 @@
-<script setup>
-</script>
-
 <template>
   <div>
     <h1>Q1</h1>
+    <QuestionForm />
   </div>
 </template>
+
+<script setup>
+import QuestionForm from "../components/questions/QuestionForm.vue"
+
+</script>
