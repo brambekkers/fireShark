fb8568edcbae75feeca95398b907e60f3db1fe58
Yesim <yesim.oguz@nl.abnamro.com>
2024-02-05 13:55:30 +0100

    Added dummy data and rendered image and question

--- src/components/questions/QuestionHeader.vue
+++ src/components/questions/QuestionHeader.vue
@@ -1,14 +1,41 @@
 <script setup>
-defineProps({
-  questions: Array,
-})
+//import { storeToRefs } from 'pinia';
+// import { useQuestionStore } from '@stores/question';
+import { ref } from "vue";
+
+const selectedQuestion = ref( {
+    "type": "singleChoice",
+    "subject": "Javascript",
+    "headerImg": "../images/background/javascript_code.png",
+    "question": "In which year was Javascript developed",
+    "answers": [
+      {
+        "text": "1994",
+        "value": false
+      },
+      {
+        "text": "1995",
+        "value": true
+      },
+      {
+        "text": "1996",
+        "value": false
+      },
+      {
+        "text": "1997",
+        "value": false
+      }
+    ]
+  })
+
+// const {selectedQuestion} = storeToRefs(useQuestionStore());
 </script>
 
 <template>
-    <div v-for="question in questions">
-       <img :src="question.headerImg"/>
-      <p>{{ question.question }}</p>
-    </div>
+      <div>
+       <img :src="selectedQuestion.headerImg"/>
+       <p>{{ selectedQuestion.question }}</p>
+      </div>
 </template>
 
 <style lang="scss" scoped>
