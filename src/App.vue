9ad01111c2ce1867904e37ed73fd593c33540337
Yesim <yesim.oguz@nl.abnamro.com>
2024-02-05 12:54:05 +0100

    Questions are fetched and QuestionHeader.vue component is created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import MainLayout from '@/layouts/MainLayout.vue'
+  import QuestionPage from './pages/QuestionPage.vue';
 </script>
 
 <template>
   <MainLayout class="bg-sharp-body-bg">
     <router-view />
+    <QuestionPage/>
   </MainLayout>
 </template>
