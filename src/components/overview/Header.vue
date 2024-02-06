88ed6920908e05af20c39174b8d95365ef387b6b
BramBekkers <bram.bekkers@outlook.com>
2024-02-06 15:42:56 +0100

    add background image header

--- src/components/overview/Header.vue
+++ src/components/overview/Header.vue
@@ -10,21 +10,32 @@ const { user } = storeToRefs(useUserStore());
     <div
       aria-hidden="true"
       style="clip-path: ellipse(130% 248% at 50% -150%)"
-      class="bg-secondary absolute inset-0"
+      class="bg-secondary background absolute inset-0"
     ></div>
 
     <section
       class="h-44 flex justify-between relative z-10 pt-8 max-w-screen-lg mx-auto"
     >
       <div class="mt-4">
-        <h1 class="font-extrabold text-2xl text-white">
-          Hi {{ user.name }}!
-        </h1>
+        <h1 class="font-extrabold text-2xl text-white">Hi {{ user.name }}!</h1>
         <p class="text-xl font-bold text-white">
-          Welcome back.
+          {{ $t('overview.welcomeBack') }}
         </p>
       </div>
       <ImageProgress />
     </section>
   </div>
 </template>
+
+<style scoped>
+.background {
+  background: linear-gradient(
+      0deg,
+      rgba(0, 146, 134, 0.95),
+      rgba(0, 94, 93, 0.95)
+    ),
+    url('@/assets/backgroundHeader.jpg');
+  background-size: cover;
+  background-position: 50%;
+}
+</style>
