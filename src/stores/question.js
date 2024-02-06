31c8d678c48df7032ce5c57e611b3943945d6de8
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-02-06 10:21:09 +0100

    Fix most of the errors

--- src/stores/question.js
+++ src/stores/question.js
@@ -9,19 +9,18 @@ export const useQuestionStore = defineStore('question', () => {
 
   async function getQuestions() {
     try {
-      return await fetch("http://192.168.0.192:3000/questions")
-        .then((res) => res.json())
-        .then((data) => {
-          const randomNr = rand(0, selectedTopics.value.length);
-          const topic = selectedTopics.value[randomNr];
-          selectedQuestions.value = data?.subjects?.[topic];
-          console.log(data?.subjects?.[topic].questions);
+      const res = await fetch("http://192.168.0.192:3000/questions");
+      const data = await res.json();
+      const randomNr = rand(0, selectedTopics.value.length - 1);
+      const topic = selectedTopics.value[randomNr];
 
-          const randomNumber = rand(0, data?.subjects?.[topic].questions.length);
-          selectedQuestion.value = data?.subjects?.[topic].questions[randomNumber];
-        })
+      console.log(selectedTopics.value, randomNr, topic)
+      selectedQuestions.value = data?.subjects?.[topic];
+      console.log(data?.subjects)
+      const randomNumber = rand(0, data?.subjects?.[topic]?.questions.length - 1);
+      selectedQuestion.value = data?.subjects?.[topic].questions[randomNumber];
     } catch (error) {
-      console.log(error)
+      console.log("error", error)
     }
   }
 
