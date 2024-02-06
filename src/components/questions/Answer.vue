1c69d764d2a806d24d2e9712083e0ed41491bd8e
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-02-06 12:09:26 +0100

    QuickFix label for and input id logic

--- src/components/questions/Answer.vue
+++ src/components/questions/Answer.vue
@@ -4,12 +4,15 @@
 </template>
 
 <script setup>
+import {computed} from 'vue';
+
 const props = defineProps({
   answer: { type: Object, required: true },
   type: { type: String, required: true },
 });
 
 const inputType = props?.type === 'singleChoice' ? 'radio' : 'checkbox';
-const { text, value } = props?.answer || '';
-const questionId = `q-${props?.text}`;
+const text = computed(() => props?.answer.text);
+const value = computed(() => props?.answer.value);
+const questionId = `q-${text.value}`;
 </script>
