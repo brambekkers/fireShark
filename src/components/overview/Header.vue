fa1ff264f5e69d4ee596d0a098a60b25fa36182d
BramBekkers <bram.bekkers@outlook.com>
2024-02-06 15:57:22 +0100

    Dynamic page titles

--- src/components/overview/Header.vue
+++ src/components/overview/Header.vue
@@ -1,8 +1,32 @@
 <script setup>
 import ImageProgress from '@/components/overview/ImageProgress.vue';
 import useUserStore from '@/stores/userStore';
+import { useRoute } from 'vue-router';
 
 const { user } = storeToRefs(useUserStore());
+const route = useRoute();
+const { t } = useI18n();
+
+const title = computed(() => {
+  if (route.name === 'Overview' || route.name === 'Settings') {
+    return `${t('general.hi')} ${user.value.name}!`;
+  }
+  if (route.name === 'Duel') {
+    return `Duelscore: 1.202`;
+  }
+});
+
+const subtitle = computed(() => {
+  if (route.name === 'Overview') {
+    return `${t('overview.welcomeBack')}!`;
+  }
+  if (route.name === 'Duel') {
+    return `${t('duel.rdyToDuel')}`;
+  }
+  if (route.name === 'Settings') {
+    return `${t('settings.changeYourProfileHere')}`;
+  }
+});
 </script>
 
 <template>
@@ -17,12 +41,11 @@ const { user } = storeToRefs(useUserStore());
       class="h-44 flex justify-between relative z-10 pt-8 max-w-screen-lg mx-auto"
     >
       <div class="mt-4">
-        <h1 class="font-extrabold text-2xl text-white">Hi {{ user.name }}!</h1>
-        <p class="text-xl font-bold text-white">
-          {{ $t('overview.welcomeBack') }}
+        <h1 class="font-extrabold text-3xl text-white">{{ title }}</h1>
+        <p class="text-2xl text-white">
+          {{ subtitle }}
         </p>
       </div>
-      <ImageProgress />
     </section>
   </div>
 </template>
