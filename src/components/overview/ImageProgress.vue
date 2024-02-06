1e011ebf08227f035d8b3a7ff76c061e94c672d3
BramBekkers <bram.bekkers@outlook.com>
2024-02-06 10:05:18 +0100

    move database

--- src/components/overview/ImageProgress.vue
+++ src/components/overview/ImageProgress.vue
@@ -1,7 +1,5 @@
+<script setup></script>
+
 <template>
   <div class="rounded-full h-36 w-36 bg-accent shadow-2xl"></div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
